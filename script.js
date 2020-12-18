5f1d766abc72270c4c23fe1358964160cb3e9651
Brian crisp <bcrisp084@yahoo.com>
2020-12-17 19:05:56 -0500

    added timer function

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-var question1 = 'what are the building blocks of a webpage?
+const startTimer = document.querySelector(".countdown");
+let secondsLeft = 60;
+function setTime() {
+  var timerInterval = setInterval(function () {
+    secondsLeft--;
+    startTimer.textContent = secondsLeft;
+    console.log(setTime);
+    if (secondsLeft === 0) {
+      clearInterval(timerInterval);
+      sendMessage();
+    }
+  }, 1000);
+}
+setTime();
