40bc62c1c7e3f3e76b6442187b1149f79381ac7a
Brian crisp <bcrisp084@yahoo.com>
2020-12-28 18:20:33 -0500

    fixed bugs in javascript

--- score.js
+++ score.js
@@ -1,11 +1,23 @@
 var returnHome = document.querySelector("#home");
 var clearBtn = document.querySelector("#clear");
+var list = document.getElementById("list");
 
 returnHome.addEventListener("click", function () {
   location.href = "index.html";
 });
 
 clearBtn.addEventListener("click", function () {
-  highScoresArea.innerHTML = "";
   window.localStorage.clear();
+  list.innerHTML = "";
 });
+
+function displayScores() {
+  var highscores = JSON.parse(localStorage.getItem("highscores"));
+  for (let i = 0; i < highscores.length; i++) {
+    var li = document.createElement("li");
+    const element = highscores[i];
+    li.innerHTML = element.initials + element.score;
+    list.append(li);
+  }
+}
+displayScores();
